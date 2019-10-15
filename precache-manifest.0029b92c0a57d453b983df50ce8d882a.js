4fbec90ac9a4013c05620531c133999f9db6873b
levani <levanbaidoshvili@gmail.com>
2019-10-15 18:06:20 +0400

    Updates

---
+++ precache-manifest.0029b92c0a57d453b983df50ce8d882a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82edaf40929074966b62550a77e3b4eb",
+    "url": "/archos/index.html"
+  },
+  {
+    "revision": "85eee627ac85f66c1ca5",
+    "url": "/archos/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "9fc72550c429862f902d",
+    "url": "/archos/static/css/main.8daeca2d.chunk.css"
+  },
+  {
+    "revision": "85eee627ac85f66c1ca5",
+    "url": "/archos/static/js/2.644c11ae.chunk.js"
+  },
+  {
+    "revision": "9fc72550c429862f902d",
+    "url": "/archos/static/js/main.6282cd0f.chunk.js"
+  },
+  {
+    "revision": "92f5ad2ee1d7e6e0bbf0",
+    "url": "/archos/static/js/runtime-main.450ff823.js"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/archos/static/media/hamburger.86595150.svg"
+  },
+  {
+    "revision": "3eecde3161ce41cf0da0e867b7018092",
+    "url": "/archos/static/media/logo.3eecde31.svg"
+  }
+]);
