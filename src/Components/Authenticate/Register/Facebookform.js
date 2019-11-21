d535b059ac52b3140a8baacc48958e0213392c91
unknown <levanbaidoshvili@gmail.com>
2019-11-22 03:10:57 +0400

    'asd'

--- src/Components/Authenticate/Register/Facebookform.js
+++ src/Components/Authenticate/Register/Facebookform.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
+import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
 
 export default class Facebookform extends Component {
+  callback = response => {
+    console.log(response);
+  };
   render() {
     return (
       <div style={{ left: this.props.left + "%" }} className="registerpageform">
+        <FacebookLogin
+          appId="553096981924816"
+          autoLoad={true}
+          callback={this.callback}
+          render={renderprops => {
+            return (
+              <div onClick={renderprops.onClick} className="facebookbutton">
+                Login With Facebook
+              </div>
+            );
+          }}
+        ></FacebookLogin>
         {["Username", "First Name", "Last Name", "Email", "Phone Number"].map(
           (a, b) => {
             return (
